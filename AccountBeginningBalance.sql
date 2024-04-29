3e0f8eb75156ad33a1120966047210ca02650c59
Muhammad Harris <96321391+haris051@users.noreply.github.com>
2024-04-29 21:10:13 +0500

    Add files via upload

--- AccountBeginningBalance.sql
+++ AccountBeginningBalance.sql
@@ -62,31 +62,36 @@ end if;
 
 
 if 
-		glFlag = 15 OR glFLAG = '511' OR glFlag= 20 OR glFlag=31 OR glFlag=34 OR 
-		glFlag=38 OR glFlag=40 OR glFlag=42 OR glFlag=44 OR 
-		glFlag=79 OR glFlag=80 OR glFlag=81 OR glFlag=46 OR 
-		glFlag=47 OR glFlag=50 OR glFlag=51 OR glFlag=54 OR 
-		glFlag=56 OR glFlag=86 OR glFlag=87 OR glFlag=85 OR 
-		glFlag=58 OR glFlag=60 OR glFlag=150 OR glFlag=151 OR 
-		glFlag=62 OR glFlag=65 OR glFlag=70 OR glFlag=68 OR 
-		glFlag=72 OR glFlag=73 OR glFlag=76 OR glFlag=77 OR 
-		glFlag=78 OR glFlag=101 OR glFlag=23 OR glFlag=104 OR 
-		glFlag=29  OR glFlag=109 OR glFlag=111 OR 
-		glFlag=114 OR glFlag=106  OR glFlag=108 or glFlag= 5551 OR glFlag= 201 or glFlag = '511'
-		OR glFlag=28 OR glFlag = 204
+		glFlag = 511 OR glFlag = 15 OR glFlag = 512 OR glFlag= 20 OR
+		glFlag = 31 OR glFlag = 34 OR glFlag = 38 OR glFlag = 40  OR 
+		glFlag = 42 OR glFlag = 44 OR glFlag = 79 OR glFlag =80  OR 
+		glFlag = 81 OR glFlag = 46 OR glFlag = 47 OR glFlag =50 OR 
+		glFlag = 51 OR glFlag = 54 OR glFlag = 56 OR glFlag = 86 OR 
+		glFlag = 87 OR glFlag = 85 OR glFlag = 58 OR glFlag = 60 OR 
+		glFlag = 150 OR glFlag = 151 OR glFlag = 62 glFlag = 65 OR 
+		glFlag = 68 OR glFlag = 70 OR glFlag = 72 OR glFlag = 73 OR 
+		glFlag = 76 OR glFlag = 78 OR glFlag = 77 OR glFlag = 101 OR 
+		glFlag = 23 OR glFlag = 102 OR glFlag = 104 OR glFlag = 106 OR
+		glFlag = 29 OR glFlag = 28 OR glFlag = 108 OR glFlag = 109 OR 
+		glFlag = 111 OR glFlag = 114 OR glFlag = 5552 OR glFlag = 115 OR 
+		glFlag = 90
     
     then
 		set debitCreditFlag = 'Credit';
 elseif 
-		glFlag = 16 OR glFlag = '510' OR glFlag =19 OR glFlag =32 OR glFlag =33 OR 
-		glFlag =37 OR glFlag =39 OR glFlag =41 OR glFlag =43 OR 
-		glFlag =45 OR glFlag =48 OR glFlag =82 OR glFlag =83 OR 
-		glFlag =84 OR glFlag =49 OR glFlag =52 OR glFlag =100 OR 
-		glFlag =53 OR glFlag =55 OR glFlag =57 OR glFlag =59 OR 
-		glFlag =64 OR glFlag =66 OR glFlag =69 OR glFlag =67 OR 
-		glFlag =71 OR glFlag =74 OR glFlag =75 OR glFlag =26 OR glFlag =205 OR
-		glFlag =102 OR glFlag = 203 OR glFlag =103 OR glFlag =105 OR glFlag =112 OR 
-		glFlag =107 OR glFlag =110 OR glFlag =113    OR glFlag= '5552'OR glFlag= '510'
+
+		glFlag = 510 OR glFlag = 16 OR glFlag = 513 OR glFlag = 19 OR 
+		glFlag = 32 OR glFlag =33 OR glFlag = 37 OR glFlag =39 OR 
+		glFlag =41 OR glFlag =43 OR glFlag =45 OR glFlag =48 OR 
+		glFlag =82 OR glFlag =83 OR glFlag =84 OR glFlag = 49 OR 
+		glFlag = 52 OR glFlag = 100 OR glFlag =53 OR glFlag = 55 OR 
+		glFlag = 57 OR glFlag = 59 OR glFlag = 64 OR glFlag = 66 OR 
+		glFlag = 67 OR glFlag = 69 OR glFlag = 71 OR glFlag = 74 OR 
+		glFlag = 75 OR glFlag = 26 OR glFlag = 201 OR glFlag = 203 OR 
+		glFlag = 103 OR glFlag = 105 OR glFlag = 107 OR glFlag = 204 OR 
+		glFlag = 205 OR glFlag = 110 OR glFlag = 113 OR glFlag = 112 OR 
+		glFlag = 5551 OR glFlag = 89 OR glFlag =116 OR glFlag = 117
+		
      then
 		set debitCreditFlag = 'Debit';
 elseif
