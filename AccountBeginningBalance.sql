1050158a947841ed4916d35bf5750cd9405d62bc
Muhammad Harris <96321391+haris051@users.noreply.github.com>
2025-02-05 17:20:53 +0500

    Update AccountBeginningBalance.sql (2-05-2025)

--- AccountBeginningBalance.sql
+++ AccountBeginningBalance.sql
@@ -1,3 +1,4 @@
+
 /*
 drop table  if Exists Daily_Account_Balance;
 
@@ -38,6 +39,9 @@ Add index Index_id(id);
 Alter Table daily_account_balance
 Add constraint AccountId_ForeignKey Foreign key(AccountId) references accounts_id(id) on delete cascade;
 
+Alter table daily_account_balance
+Add Constraint DAILY_ACCOUNT_BALANCE_UNIQUE Unique key(`ACCOUNTID`,`ENTRYDATE`);
+
 
 drop FUNCTION  if Exists FUNC_SET_DAILY_ACCOUNT_BALANCE;
 
@@ -49,85 +53,85 @@ READS SQL DATA
 DETERMINISTIC
 BEGIN
 
-Declare getAccountType int;
-Declare debitCreditFlag Text;
-Declare OpenningBalanceFlag Text;
-Declare Unique_AccountId_EntryDate_PrimaryKey int;
+	Declare getAccountType int;
+	Declare debitCreditFlag Text;
+	Declare OpenningBalanceFlag Text;
+	Declare Unique_AccountId_EntryDate_PrimaryKey int;
 
-if  accountId is null
-	then
-		return 'accountID is null';
-elseif glFlag is null
-	then
-		return 'GLFLAG is null';
-elseif
-    entryDate ='' or 
-    entryDate is null
-	then return 'EntryDate is NULL';	
-end if;
-
-
-select      account_type.ACCOUNT_ID into getAccountType
-from        accounts_id 
-inner join  account_type 
-on 
-			account_type.id=accounts_id.ACCOUNT_TYPE_ID 
-where 
-			accounts_id.id=accountId;
+	if  accountId is null
+		then
+			return 'accountID is null';
+	elseif glFlag is null
+		then
+			return 'GLFLAG is null';
+	elseif
+		entryDate ='' or 
+		entryDate is null
+		then return 'EntryDate is NULL';	
+	end if;
 
 
-if
-        getAccountType = '' or
-        getAccountType is null
-        then
-        return 'Account Type is Null';
-    
-end if;
+	select      account_type.ACCOUNT_ID into getAccountType
+	from        accounts_id 
+	inner join  account_type 
+	on 
+				account_type.id=accounts_id.ACCOUNT_TYPE_ID 
+	where 
+				accounts_id.id=accountId;
 
 
-if 
-		glFlag = 511 OR glFlag = 15 OR glFlag = 512 OR glFlag= 20 OR
-		glFlag = 31 OR glFlag = 34 OR glFlag = 38 OR glFlag = 40  OR 
-		glFlag = 42 OR glFlag = 44 OR glFlag = 79 OR glFlag =80  OR 
-		glFlag = 81 OR glFlag = 46 OR glFlag = 47 OR glFlag =50 OR 
-		glFlag = 51 OR glFlag = 54 OR glFlag = 56 OR glFlag = 86 OR 
-		glFlag = 87 OR glFlag = 85 OR glFlag = 58 OR glFlag = 60 OR 
-		glFlag = 150 OR glFlag = 151 OR glFlag = 62 OR glFlag = 65 OR 
-		glFlag = 68 OR glFlag = 70 OR glFlag = 72 OR glFlag = 73 OR 
-		glFlag = 76 OR glFlag = 78 OR glFlag = 77 OR glFlag = 101 OR 
-		glFlag = 23 OR glFlag = 102 OR glFlag = 104 OR glFlag = 106 OR
-		glFlag = 5554 OR glFlag = 29 OR glFlag = 28 OR glFlag = 108 OR 
-	    glFlag = 109 OR glFlag = 111 OR glFlag = 114 OR glFlag = 5552 OR 
-	    glFlag = 115 OR glFlag = 90 OR glFLAG = 5557 OR glFlag = 5558
-    
-    then
-		set debitCreditFlag = 'Credit';
-elseif 
-
-		glFlag = 510 OR glFlag = 16 OR glFlag = 513 OR glFlag = 19 OR 
-		glFlag = 32 OR glFlag =33 OR glFlag = 37 OR glFlag =39 OR 
-		glFlag =41 OR glFlag =43 OR glFlag =45 OR glFlag =48 OR 
-		glFlag =82 OR glFlag =83 OR glFlag =84 OR glFlag = 49 OR 
-		glFlag = 52 OR glFlag = 100 OR glFlag =53 OR glFlag = 55 OR 
-		glFlag = 57 OR glFlag = 59 OR glFlag = 64 OR glFlag = 66 OR 
-		glFlag = 67 OR glFlag = 69 OR glFlag = 71 OR glFlag = 74 OR 
-		glFlag = 75 OR glFlag = 26 OR glFlag = 201 OR glFlag = 203 OR 
-		glFlag = 103 OR glFlag = 105 OR glFlag = 5553 OR glFlag = 107 OR 
-	    glFlag = 204 OR glFlag = 205 OR glFlag = 110 OR glFlag = 113 OR 
-	    glFlag = 112 OR glFlag = 5551 OR glFlag = 89 OR glFlag =116 OR 
-	    glFlag = 117  OR glFlag = 5556 OR glFlag = 5559
+	if
+			getAccountType = '' or
+			getAccountType is null
+			then
+			return 'Account Type is Null';
 		
+	end if;
+
+
+	if 
+			glFlag = 511 OR glFlag = 15 OR glFlag = 512 OR glFlag= 20 OR
+			glFlag = 31 OR glFlag = 34 OR glFlag = 38 OR glFlag = 40  OR 
+			glFlag = 42 OR glFlag = 44 OR glFlag = 79 OR glFlag =80  OR 
+			glFlag = 81 OR glFlag = 46 OR glFlag = 47 OR glFlag =50 OR 
+			glFlag = 51 OR glFlag = 54 OR glFlag = 56 OR glFlag = 86 OR 
+			glFlag = 87 OR glFlag = 85 OR glFlag = 58 OR glFlag = 60 OR 
+			glFlag = 150 OR glFlag = 151 OR glFlag = 62 OR glFlag = 65 OR 
+			glFlag = 68 OR glFlag = 70 OR glFlag = 72 OR glFlag = 73 OR 
+			glFlag = 76 OR glFlag = 78 OR glFlag = 77 OR glFlag = 101 OR 
+			glFlag = 23 OR glFlag = 102 OR glFlag = 104 OR glFlag = 106 OR
+			glFlag = 5554 OR glFlag = 29 OR glFlag = 28 OR glFlag = 108 OR 
+			glFlag = 109 OR glFlag = 111 OR glFlag = 114 OR glFlag = 5552 OR 
+			glFlag = 115 OR glFlag = 90 OR glFLAG = 5557 OR glFlag = 5558
 		
-     then
-		set debitCreditFlag = 'Debit';
-elseif
-	glFlag = 5555 OR glFlag = -5555
+		then
+			set debitCreditFlag = 'Credit';
+	elseif 
+
+			glFlag = 510 OR glFlag = 16 OR glFlag = 513 OR glFlag = 19 OR 
+			glFlag = 32 OR glFlag =33 OR glFlag = 37 OR glFlag =39 OR 
+			glFlag =41 OR glFlag =43 OR glFlag =45 OR glFlag =48 OR 
+			glFlag =82 OR glFlag =83 OR glFlag =84 OR glFlag = 49 OR 
+			glFlag = 52 OR glFlag = 100 OR glFlag =53 OR glFlag = 55 OR 
+			glFlag = 57 OR glFlag = 59 OR glFlag = 64 OR glFlag = 66 OR 
+			glFlag = 67 OR glFlag = 69 OR glFlag = 71 OR glFlag = 74 OR 
+			glFlag = 75 OR glFlag = 26 OR glFlag = 201 OR glFlag = 203 OR 
+			glFlag = 103 OR glFlag = 105 OR glFlag = 5553 OR glFlag = 107 OR 
+			glFlag = 204 OR glFlag = 205 OR glFlag = 110 OR glFlag = 113 OR 
+			glFlag = 112 OR glFlag = 5551 OR glFlag = 89 OR glFlag =116 OR 
+			glFlag = 117  OR glFlag = 5556 OR glFlag = 5559
+			
+			
+		 then
+			set debitCreditFlag = 'Debit';
+	elseif
+		glFlag = 5555 OR glFlag = -5555
 		 then
 			set OpenningBalanceFlag = 'OpenningBalance';
-		
-end if;
+			
+	end if;
 
-if OpenningBalanceFlag = 'OpenningBalance'
+	if OpenningBalanceFlag = 'OpenningBalance'
 		then
 			if 
 			  getAccountType = 3 OR 
@@ -302,84 +306,85 @@ if OpenningBalanceFlag = 'OpenningBalance'
 
 	end if;
 
-if 
-	getAccountType = 3 or 
-	getAccountType = 2 or 
-    getAccountType = 5 
-    then
-		if
-		  debitCreditFlag='Debit'
-				then
-					
-					select A.id INTO Unique_AccountId_EntryDate_PrimaryKey 
-					from Daily_Account_Balance as A 
-					where A.AccountId = accountId 
-					and Convert(A.EntryDate,Date) = convert(entryDate,Date);
-						
-					if (Unique_AccountId_EntryDate_PrimaryKey is null or Unique_AccountId_EntryDate_PrimaryKey = '')
-						then 
-							insert into Daily_Account_Balance(AccountId,Debit,Credit,Balance,EntryDate) 
-							values(accountId,amount,0,amount,convert(entryDate,date));	
-                        else 
-							update Daily_Account_Balance SET Balance=Balance+amount,Debit=Debit+amount where id = Unique_AccountId_EntryDate_PrimaryKey;
-							
-					end if;
-					
-                    return '';
-				else
-				
-					select A.id INTO Unique_AccountId_EntryDate_PrimaryKey 
-					from Daily_Account_Balance as A 
-					where A.AccountId = accountId 
-					and Convert(A.EntryDate,Date) = convert(entryDate,Date);
-
-                    if(Unique_AccountId_EntryDate_PrimaryKey is null or Unique_AccountId_EntryDate_PrimaryKey = '')
-						then 
-							insert into Daily_Account_Balance(AccountId,Debit,Credit,Balance,EntryDate) values(accountId,0,amount,amount*-1,convert(entryDate,date));
-						else 
-							update Daily_Account_Balance SET Balance=Balance-amount,Credit=Credit+amount where id = Unique_AccountId_EntryDate_PrimaryKey;
-				    end if;
-					
-                    return '';
-		end if;
-elseif 
-	  getAccountType = 1 or 
-	  getAccountType = 4 or 
-      getAccountType = 6
+	if 
+		getAccountType = 3 or 
+		getAccountType = 2 or 
+		getAccountType = 5 
 		then
-			if 
-			  debitCreditFlag = 'Debit'
-              then
-				  
-				  select A.id INTO Unique_AccountId_EntryDate_PrimaryKey 
-				  from Daily_Account_Balance as A 
-				  where A.AccountId = accountId 
-				  and Convert(A.EntryDate,Date) = convert(entryDate,Date);
-				  
-				  if(Unique_AccountId_EntryDate_PrimaryKey is null or Unique_AccountId_EntryDate_PrimaryKey = '')
-						then 
-							insert into Daily_Account_Balance(AccountId,DEBIT,CREDIT,Balance,EntryDate) values(accountId,amount,0,amount*-1,convert(entryDate,date));
-						else 
-							update Daily_Account_Balance SET Balance=Balance-amount,DEBIT = DEBIT + amount where id = Unique_AccountId_EntryDate_PrimaryKey;
-				  end if;
-				  
-                  return '';
-			  else
-				  select A.id INTO Unique_AccountId_EntryDate_PrimaryKey 
-				  from Daily_Account_Balance as A  
-				  where A.AccountId = accountId 
-				  and Convert(A.EntryDate,Date) = convert(entryDate,Date);
-				  
-				  if(Unique_AccountId_EntryDate_PrimaryKey is null or Unique_AccountId_EntryDate_PrimaryKey = '')
-						then
-							insert into Daily_Account_Balance(AccountId,DEBIT,CREDIT,Balance,EntryDate) values(accountId,0,amount,amount,convert(entryDate,date));
-						else 
-							update Daily_Account_Balance SET Balance=Balance+amount,CREDIT = CREDIT + amount where id =Unique_AccountId_EntryDate_PrimaryKey;
-				  end if;
-				 
-                  return '';
+			if
+			  debitCreditFlag='Debit'
+					then
+						
+						select A.id INTO Unique_AccountId_EntryDate_PrimaryKey 
+						from Daily_Account_Balance as A 
+						where A.AccountId = accountId 
+						and Convert(A.EntryDate,Date) = convert(entryDate,Date);
+							
+						if (Unique_AccountId_EntryDate_PrimaryKey is null or Unique_AccountId_EntryDate_PrimaryKey = '')
+							then 
+								insert into Daily_Account_Balance(AccountId,Debit,Credit,Balance,EntryDate) 
+								values(accountId,amount,0,amount,convert(entryDate,date));	
+							else 
+								update Daily_Account_Balance SET Balance=Balance+amount,Debit=Debit+amount where id = Unique_AccountId_EntryDate_PrimaryKey;
+								
+						end if;
+						
+						return '';
+					else
+					
+						select A.id INTO Unique_AccountId_EntryDate_PrimaryKey 
+						from Daily_Account_Balance as A 
+						where A.AccountId = accountId 
+						and Convert(A.EntryDate,Date) = convert(entryDate,Date);
+
+						if(Unique_AccountId_EntryDate_PrimaryKey is null or Unique_AccountId_EntryDate_PrimaryKey = '')
+							then 
+								insert into Daily_Account_Balance(AccountId,Debit,Credit,Balance,EntryDate) values(accountId,0,amount,amount*-1,convert(entryDate,date));
+							else 
+								update Daily_Account_Balance SET Balance=Balance-amount,Credit=Credit+amount where id = Unique_AccountId_EntryDate_PrimaryKey;
+						end if;
+						
+						return '';
 			end if;
-end if;
+	elseif 
+		  getAccountType = 1 or 
+		  getAccountType = 4 or 
+		  getAccountType = 6
+			then
+				if 
+				  debitCreditFlag = 'Debit'
+				  then
+					  
+					  select A.id INTO Unique_AccountId_EntryDate_PrimaryKey 
+					  from Daily_Account_Balance as A 
+					  where A.AccountId = accountId 
+					  and Convert(A.EntryDate,Date) = convert(entryDate,Date);
+					  
+					  if(Unique_AccountId_EntryDate_PrimaryKey is null or Unique_AccountId_EntryDate_PrimaryKey = '')
+							then 
+								insert into Daily_Account_Balance(AccountId,DEBIT,CREDIT,Balance,EntryDate) values(accountId,amount,0,amount*-1,convert(entryDate,date));
+							else 
+								update Daily_Account_Balance SET Balance=Balance-amount,DEBIT = DEBIT + amount where id = Unique_AccountId_EntryDate_PrimaryKey;
+					  end if;
+					  
+					  return '';
+				  else
+					  select A.id INTO Unique_AccountId_EntryDate_PrimaryKey 
+					  from Daily_Account_Balance as A  
+					  where A.AccountId = accountId 
+					  and Convert(A.EntryDate,Date) = convert(entryDate,Date);
+					  
+					  if(Unique_AccountId_EntryDate_PrimaryKey is null or Unique_AccountId_EntryDate_PrimaryKey = '')
+							then
+								insert into Daily_Account_Balance(AccountId,DEBIT,CREDIT,Balance,EntryDate) values(accountId,0,amount,amount,convert(entryDate,date));
+							else 
+								update Daily_Account_Balance SET Balance=Balance+amount,CREDIT = CREDIT + amount where id =Unique_AccountId_EntryDate_PrimaryKey;
+					  end if;
+					 
+					  return '';
+				end if;
+	end if;
+	
 END$$
 DELIMITER ;
 
